48494caad15b701a63a43648b2a3b54d4887f594
grigorii <grigoriiborodin@yandex.ru>
2022-07-28 11:59:09 +0300

    initial commit

---
+++ src/main/java/com/flag/flagapp/FlagAppApplication.java
@@ -0,0 +1,13 @@
+package com.flag.flagapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlagAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlagAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flag/flagapp/FlagAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flag.flagapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlagAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
